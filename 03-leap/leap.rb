2bc7ec0b8698054520b43707b1c20d510b3bfd32
Jiajia Yang <jocelynyang2016@gmail.com>
2024-01-30 11:51:13 -0600

    change to simpler version

--- 03-leap/leap.rb
+++ 03-leap/leap.rb
@@ -1,22 +1,31 @@
 class Year
 
   def self.leap?(year)
-    leap = false
-    if year % 4 == 0
-      if year % 100 == 0
-        if year % 400 == 0
-          leap = true
-        else
-          leap = false
-        end
-      else
-        leap = true
-      end
-    else
-      leap = false
-    end
-    return leap
+    # simple solution
+    return true if year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)
   end
-
 end
+
+# original code
+# class Year
+
+#   def self.leap?(year)
+#     leap = false
+#     if year % 4 == 0
+#       if year % 100 == 0
+#         if year % 400 == 0
+#           leap = true
+#         else
+#           leap = false
+#         end
+#       else
+#         leap = true
+#       end
+#     else
+#       leap = false
+#     end
+#     return leap
+#   end
+
+# end
 
